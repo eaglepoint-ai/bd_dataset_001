56759ea35910c80019037f90a80d5740864ef08e
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-01-17 17:16:26 +0300

    Limit PR scope to SongList refactor directory

--- dbd253-Log_parser_optimization/src/main/log_parser/compile_and_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Compiling Evaluation...
-javac evaluation/Evaluation.java
-if %errorlevel% neq 0 (
-    echo Compilation failed!
-    exit /b %errorlevel%
-)
-echo Running Evaluation...
-java -cp evaluation Evaluation
